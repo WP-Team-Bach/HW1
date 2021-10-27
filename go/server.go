43e8a77aacbb3c71eef8a38f80cb188e85e3c431
arhemd <mehrad.dadar@gmail.com>
2021-10-27 05:39:05 +0330

    hw implementation completed

--- go/server.go
+++ go/server.go
@@ -1,18 +1,62 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"crypto/sha256"
+
+	"github.com/gin-gonic/gin"
+
+	"context"
+
+	"github.com/go-redis/redis/v8"
+
+	b64 "encoding/base64"
+)
+
+var ctx = context.Background()
+var rdb *redis.Client
 
 func main() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+	})
+
 	r := gin.Default()
 
-	r.GET("/", RequestHandler)
+	r.POST("/sha256", PutHandler)
+	r.GET("/sha256", GetHandler)
 	r.NoRoute(NotFoundHandler)
 
-	r.Run("0.0.0.0:8080")
+	r.Run(":8080")
 }
 
-func RequestHandler(c *gin.Context) {
-	c.JSON(200, gin.H{"result": "Hellgo"})
+func PutHandler(c *gin.Context) {
+	str := c.PostForm("str")
+	if len(str) < 8 {
+		c.JSON(400, gin.H{"error": "400"})
+		return
+	}
+	h := sha256.New()
+	h.Write([]byte(str))
+	b := h.Sum(nil)
+	sha := b64.StdEncoding.EncodeToString(b)
+	rdb.Set(ctx, sha, str, 0)
+	c.JSON(200, gin.H{"result": sha})
+}
+
+func GetHandler(c *gin.Context) {
+	sha := c.Query("sha")
+	if len(sha) < 44 {
+		c.JSON(400, gin.H{"error": "400"})
+		return
+	}
+	str, err := rdb.Get(ctx, sha).Result()
+	if err == redis.Nil {
+		c.JSON(200, gin.H{"result": nil})
+		return
+	}
+	c.JSON(200, gin.H{"result": str})
 }
 
 func NotFoundHandler(c *gin.Context) {
--- node/server.js
+++ node/server.js
@@ -1,10 +1,46 @@
 const express = require("express")
 const http = require("http")
+const crypto = require('crypto');
+const redis = require("redis")
+const client = redis.createClient({host: "redis"});
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+const PORT = 8080
 
 let app = express()
+app.use(express.urlencoded({extended: true}));
 
-function requestHandler(req, res){
-    res.json({result: "hello"})
+app.post('/sha256', putHandler)
+app.get('/sha256', getHandler)
+app.get('*', notFoundHandler)
+
+let httpServer = http.createServer(app)
+httpServer.listen(PORT)
+console.log(`HTTP Server listening on port: ${PORT}`)
+
+async function putHandler(req, res){
+    let str = req.body.str
+    if (str == null || str.length < 8) {
+        res.status(400)
+        res.json({error: "400"})
+        return
+    }
+    let sha = crypto.createHash('sha256').update(str).digest("base64")
+    await setAsync(sha, str)
+    res.json({result: sha})
+}
+
+async function getHandler(req, res){
+    let sha = req.query.sha
+    if (sha == null || sha.length < 44) {
+        res.status(400)
+        res.json({error: "400"})
+        return
+    }
+    let str = await getAsync(sha)
+    res.json({result: str})
 }
 
 function notFoundHandler(req, res) {
@@ -12,8 +48,3 @@ function notFoundHandler(req, res) {
     res.json({error: "404"})
 }
 
-app.get('/', requestHandler)
-app.get('*', notFoundHandler)
-
-let httpServer = http.createServer(app)
-httpServer.listen(8080)
